9bc0a3639411220d71998c10144ed080887e4b14
aziz şimşek <98578225+02AzizSimsek@users.noreply.github.com>
2023-03-04 22:07:17 +0300

    Add files via upload

---
+++ Python Programming for Data Science/part-2.py
@@ -0,0 +1,47 @@
+
+#İntroduction To Data Strcutures
+
+#number:integer
+x=46
+type(x)
+
+#number:float
+y=10.6
+type(y)
+
+#number:complex
+
+z=2j+1
+type(z)
+
+#String
+x="Hello AI era"
+type(x)
+
+#Boolean
+
+True
+False
+type(True)
+5==4
+3==2
+3==3
+
+#Lists
+
+x=["btc","eth","xrp"]
+type(x)
+
+#Dictionary
+
+x={"name":"peter","Age":36}
+type(x)
+
+#Tuple
+x=("python","ml","ds")
+type(x)
+
+#Set
+
+x={"python","ml","ds"}#the difference from the dictionary is that there is no ":"expression
+type(x)
